ceb40e3e226e0db5c91ff6d54e5fcda973bfa0b3
Andromu <andresrodriguez3d@gmail.com>
2024-08-27 08:34:51 +0100

    Add createGrid function

--- etchasketch.js
+++ etchasketch.js
@@ -1,1 +1,17 @@
 const container = document.getElementById("container");
+
+function createGrid (gridSize) {
+    for (let i = 0; i < gridSize; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        
+        for (let x = 0; x < gridSize; x++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+    }
+}
+
+createGrid(16);
